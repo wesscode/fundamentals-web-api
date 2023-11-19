25888780145fa4cb20d92272e752324bca68542d
Wesley Alves <wesley.info07@gmail.com>
2023-11-19 17:50:26 -0300

    Criando classe Produto, Configurando EntityFramework

---
+++ ApiFuncional/Data/ApiDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ApiFuncional.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+        {
+        }
+    }
+}
