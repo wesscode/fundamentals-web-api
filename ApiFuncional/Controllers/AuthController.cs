d605e0ec9e81ff2ea50e35d7be5307e1f1c38c0b
Wesley Alves <wesley.alves@sete.inf.br>
2023-11-21 12:41:07 -0300

    Criação do AuthController e seus ViewModels

---
+++ ApiFuncional/Controllers/AuthController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiFuncional.Controllers
+{
+    [ApiController]
+    [Route("api/conta")]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult> Registrar()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login()
+        {
+            return Ok();
+        }
+    }
+}
